6d800e4d8f8688c1f693f111e154008f63be50e0
huang.nicolas <huang.nicolas3wa.io>
2025-01-23 21:15:30 +0100

    Update App.vue with new modifications

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
+  <input type="number" v-model="product.quantity" />
   <h2>Prix total HT : {{ totalPriceHT }}</h2>
   <h2>Prix total TTC : {{ totalPriceTTC }}</h2>
 </template>
 
 <script setup lang="ts">
-import { computed, reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
 const product = reactive({
   name: 'books',
@@ -12,8 +13,8 @@ const product = reactive({
   priceHT: 10,
 })
 
-const totalPriceHT = computed(() => product.totalPriceHT * product.quantity)
-const totalPriceTTC = computed(() => product.totalPriceHT * product.quantity * 1.2)
+const totalPriceHT = computed(() => product.priceHT * product.quantity)
+const totalPriceTTC = computed(() => product.priceHT * product.quantity * 1.2)
 </script>
 
 <style></style>
