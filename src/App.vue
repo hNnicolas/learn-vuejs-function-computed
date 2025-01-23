f15d505a24a6fa65900bea1411f442e4b1318f32
huang.nicolas <huang.nicolas3wa.io>
2025-01-23 21:03:24 +0100

    Initial commit with project files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>Prix total HT : {{ totalPriceHT }}</h2>
+  <h2>Prix total TTC : {{ totalPriceTTC }}</h2>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+
+const product = reactive({
+  name: 'books',
+  quantity: 3,
+  priceHT: 10,
+})
+
+const totalPriceHT = computed(() => product.totalPriceHT * product.quantity)
+const totalPriceTTC = computed(() => product.totalPriceHT * product.quantity * 1.2)
+</script>
+
+<style></style>
